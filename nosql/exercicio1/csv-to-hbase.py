08ecae4d582167e98516fd6d228216263ad489e8
vini <vinicius.remigio@gmail.com>
2014-10-08 01:00:08 -0300

    codigo que gera entrada para hbase

--- nosql/exercicio1/csv-to-hbase.py
+++ nosql/exercicio1/csv-to-hbase.py
@@ -1,35 +1,30 @@
 import os.path
+import sys
 
-with open('search.csv','r') as f: 
+inputFile = sys.argv[1]
+with open(inputFile,'r') as f: 
+	
+	tableName = inputFile.split('.')[0]
+	
+	searchStr = "create '"+tableName+"' , 'CF_Data', 'CF_Meta', 'CF_Rank'"
 	
 	input = f.readline()
-	colFamilies = input.split(',')
-	
-	searchStr = "create 'search' , '{}', '{}', '{}'"
-	createSearch = searchStr.format(colFamilies[1],colFamilies[2],colFamilies[3].strip())
-	
-	colLine = f.readline()
-	cols = colLine.split(',')
+	rowkey = input.split(',')[2].strip()
 	
 	putCmdList = ""
 	
 	for line in f:
-		
 		tokens = line.split(',')
-		putCmd = "put 'search', "
-		rowkey = tokens[0]
+		putCmdList = putCmdList + "put '"+tableName+"','"+rowkey+"','"+tokens[0]+":"+tokens[1]+"','"+tokens[2].strip()+"'\n"
 		
-		for i in range(1,len(tokens)):
-			putCmd = rowkey+ ", '"+cols[i-1]+"','"+tokens[i].strip()+"'"
-		putCmdList = putCmdList + putCmd
-	
-filename = "output/search-hbase.csv"
+			
+filename = "output/"+tableName+"-hbase.csv"
 
 if not os.path.exists(os.path.dirname(filename)):
     os.makedirs(os.path.dirname(filename), 0777)
 
 with open(filename,'w+') as g:
-	g.write(createSearch)
+	g.write(searchStr)
 	g.write('\n')
 	g.write(putCmdList)
 	
